5d8b3845724ad336a72cf6badebd6dd789af41ca
HorroGlitch <lverot@edenschool.fr>
2023-11-02 14:08:54 +0100

    first algo finish

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-// console.log("Bonjour");
 
-let somme = 0;
+
 let ingéniosité = 5;
 let vitesse = 4;
 let force = 7;
@@ -12,7 +11,33 @@ let spiderMan = vitesse * 4 + 5;
 let captainAmerica = force * 3 + 7;
 let thor = foudre * 4 + 20;
 
-for (let i = 0; i < array.length; i++) {
-  const element = array[i];
-  
+let puissanceTotal = ironMan + spiderMan + captainAmerica + thor;
+
+let nbrTours = 0;
+
+function vaincreThanos(){
+  while (thanos > puissanceTotal) {
+    if(nbrTours%4 == 0){
+      ingéniosité ++;
+      ironMan = ingéniosité * 3 + 10;
+    } else if (nbrTours%4 == 1) {
+      vitesse ++;
+      spiderMan = vitesse * 4 + 5;
+    } else if (nbrTours%4 == 2) {
+      force ++;
+      captainAmerica = force * 3 + 7;
+    } else {
+      foudre ++;
+      thor = foudre * 4 + 20;
+    }
+
+    puissanceTotal = ironMan + spiderMan + captainAmerica + thor;
+    nbrTours += 1;
+  }
+  console.log(nbrTours);
 }
+vaincreThanos();
+
+
+  
+    
