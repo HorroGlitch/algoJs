0d7040962bcb960f75e5826cab0280be73a93bdd
HorroGlitch <lverot@edenschool.fr>
2023-11-02 11:09:58 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+// console.log("Bonjour");
+
+let somme = 0;
+let ingéniosité = 5;
+let vitesse = 4;
+let force = 7;
+let foudre = 4;
+let thanos = 163;
+
+let ironMan = ingéniosité * 3 + 10;
+let spiderMan = vitesse * 4 + 5;
+let captainAmerica = force * 3 + 7;
+let thor = foudre * 4 + 20;
+
+for (let i = 0; i < array.length; i++) {
+  const element = array[i];
+  
+}
